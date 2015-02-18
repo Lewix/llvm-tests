11e8f209c40a7ff5a7c56dd8fcef3bed6138417c
Lewis Brown <lewix7@hotmail.com>
2015-02-18 10:23:52 +0100

    Use convenience methods for GEP

--- exercise1.cpp
+++ exercise1.cpp
@@ -9,7 +9,15 @@
 #include "llvm/ExecutionEngine/SectionMemoryManager.h"
 #include "llvm/Support/TargetSelect.h"
 #include <stdio.h>
+#include <stdint.h>
 using namespace llvm;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int8_t i8;
+typedef uint64_t ui64;
+typedef uint32_t ui32;
+typedef uint8_t ui8;
 
 /* YT data types */
 
@@ -24,12 +32,12 @@ enum EValueType {
 };
 
 struct TValue {
-  short Id; // Column name.
-  short Type; // Column type (EValueType).
-  int Length; // For string-like values this fields contains size of variable part.
+  i8 Id; // Column name.
+  i8 Type; // Column type (EValueType).
+  i32 Length; // For string-like values this fields contains size of variable part.
   union {
-    long Int64;
-    unsigned long Uint64;
+    i64 Int64;
+    ui64 Uint64;
     double Double;
     bool Boolean;
     const char* String;
@@ -78,15 +86,13 @@ void func(TRow row, TValue* result)
   result->Data.Int64 = values[0].Data.Int64 + values[1].Data.Int64;
 }
 
-Module* funcIR(TRow row, TValue* result)
+Module* funcIR()
 {
   LLVMContext &context = getGlobalContext();
   Module* module = new Module("addition func", context);
   IRBuilder<> builder(context);
 
   FunctionType* funTp = TypeBuilder<void(TRow, TValue*), true>::get(context);
-  Type* i64Tp = TypeBuilder<types::i<64>, true>::get(context);
-  Type* i32Tp = TypeBuilder<types::i<32>, true>::get(context);
   Type* tvaluePtrTp = TypeBuilder<TValue*, true>::get(context);
 
   Function* function = Function::Create(funTp, Function::ExternalLinkage, "func", module);
@@ -101,33 +107,22 @@ Module* funcIR(TRow row, TValue* result)
   resultArg->setName("result");
 
   // TValue* values = (TValue*)(row + 1)
-  Value* one = Constant::getIntegerValue(i64Tp, APInt(64, 1));
-  Value* rowIncPtr = builder.CreateInBoundsGEP(rowArg, one);
-  Instruction::CastOps castOp = CastInst::getCastOpcode(rowIncPtr, false, tvaluePtrTp, false);
-  Value* valuesPtr = builder.CreateCast(castOp, rowIncPtr, tvaluePtrTp, "values");
+  Value* rowIncPtr = builder.CreateConstInBoundsGEP1_32(rowArg, 1);
+  Value* valuesPtr = builder.CreatePointerCast(rowIncPtr, tvaluePtrTp, "values");
 
   // i64 value0Data = values[0].Data.Int64
-  Value* indices[2] = {
-    Constant::getIntegerValue(i64Tp, APInt(64, 0)),
-    Constant::getIntegerValue(i32Tp, APInt(32, 3))
-  };
-  Value* value0DataPtr =
-    builder.CreateInBoundsGEP(valuesPtr, ArrayRef<Value*>(indices, 2));
+  Value* value0DataPtr = builder.CreateConstInBoundsGEP2_32(valuesPtr, 0, 3);
   Value* value0Data = builder.CreateLoad(value0DataPtr, "value0Data");
 
   // i64 value1Data = values[1].Data.Int64
-  indices[0] = Constant::getIntegerValue(i64Tp, APInt(64, 1));
-  Value* value1DataPtr =
-    builder.CreateInBoundsGEP(valuesPtr, ArrayRef<Value*>(indices, 2));
+  Value* value1DataPtr = builder.CreateConstInBoundsGEP2_32(valuesPtr, 1, 3);
   Value* value1Data = builder.CreateLoad(value1DataPtr, "value1Data");
 
   // i64 valueSum = value0Data + value1Data
   Value* valueSum = builder.CreateAdd(value0Data, value1Data, "valueSum");
 
   // result->Data.Int64 = resultSum;
-  indices[0] = Constant::getIntegerValue(i64Tp, APInt(64, 0));
-  Value* resultDataPtr =
-    builder.CreateInBoundsGEP(resultArg, ArrayRef<Value*>(indices, 2));
+  Value* resultDataPtr = builder.CreateConstInBoundsGEP2_32(resultArg, 0, 3);
   builder.CreateStore(valueSum, resultDataPtr);
 
   builder.CreateRetVoid();
@@ -151,12 +146,12 @@ int main(int argc, char** argv)
   values[2] = { 0, EValueType::Int64, 0, { 0 } }; // int64(0)
 
   InitializeNativeTarget();
-  Module* module = funcIR(row, &values[2]);
+  Module* module = funcIR();
   ExecutionEngine* engine = EngineBuilder(module).create();
   void* funcPtr = engine->getPointerToFunction(module->getFunction("func"));
 
   void(*llvmfunc)(TRow, TValue*) = (void (*)(TRow, TValue*))funcPtr;
   llvmfunc(row, &(values[2]));
 
-  printf("%ld\n", values[2].Data.Int64);
+  printf("%ld\n", (long)values[2].Data.Int64);
 }
